5fcc6ac3bb2217081b26e3202efc9e9f76d43632
Raymond Cheung <iray100200@hotmail.com>
2018-06-02 21:55:12 +0800

    update

--- src/com/base64.js
+++ src/com/base64.js
@@ -11,14 +11,14 @@ module.exports = Base64 = function (str) {
     }
     for (var i = 0; i < estr.length / 6; i++) {
         let s = estr.substring(i * 6, (i + 1) * 6);
-        let f = 6 - s.length;
-        s = s.concat.apply(s, Array.apply(this, { length: f }).map(Function.call, function () { return '0' }))
+        let f = 6 - s.length, g = '';
+        if (f > 0) {
+            s = s.concat.apply(s, Array.apply(this, { length: f }).map(() => '0'));
+            g = f === 2 ? '=' : f === 4 ? '==' : '';
+        }
         let p = parseInt(s, 2);
         let d = map[p];
-        result = result.concat(d);
-        if (f > 0) {
-            result = result.concat(f === 2 ? '=' : f === 4 ? '==' : '');
-        }
+        result = result.concat.call(result, d, g);
     }
     return result;
 }
